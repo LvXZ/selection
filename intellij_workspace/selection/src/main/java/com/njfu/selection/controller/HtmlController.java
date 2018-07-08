3b8c58d7ed3c72b56cb4cbe03952d7151ab079f7
lvxiaozheng <2293548359@qq.com>
2018-07-08 22:05:33 +0800

    2018-07-08 a

---
+++ intellij_workspace/selection/src/main/java/com/njfu/selection/controller/HtmlController.java
@@ -0,0 +1,42 @@
+package com.njfu.selection.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName: HtmlController
+ * @Description: 网页响应服务后端
+ * @Author: lvxz
+ * @Date: 2018-07-07  20:19
+ */
+
+@Controller
+public class HtmlController {
+
+    @RequestMapping("/")
+    public String login1Page() {
+        return "/login.html";
+    }
+
+    @RequestMapping("/login")
+    public String login2Page() {
+        return "/login.html";
+    }
+
+    @RequestMapping("/index_student")
+    public String indexStudentPage() {
+        return "/index_student.html";
+    }
+
+    @RequestMapping("/index_teacher")
+    public String indexTeacherPage() {
+        return "/index_teacher.html";
+    }
+
+    @RequestMapping("/index_admin")
+    public String indexAdminPage() {
+        return "/index_admin.html";
+    }
+
+
+}
